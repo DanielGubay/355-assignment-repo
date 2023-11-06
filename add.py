e2e22d1170218aed34bdbffb81734229278f1695
DanielGubay <danielgubay978@gmail.com>
2023-11-06 14:31:45 -0500

    add two numbers script.py

---
+++ add.py
@@ -0,0 +1,12 @@
+# Get user input for the first number
+num1 = float(input("Enter the first number: "))
+
+# Get user input for the second number
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers
+result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {result}")
+
